aab997e0aebe824793ddbdf572e9df8b1eb063fc
luxq <xueqian1991@gmail.com>
2023-03-25 20:23:12 +0800

    update response format

--- service/handler/base.go
+++ service/handler/base.go
@@ -13,9 +13,9 @@ func (d *BaseController) ResponseInfo(code int, errMsg interface{}, result inter
 	//d.Data["json"] = map[string]interface{}{"error": codestr, "err_msg": errMsg, "data": result}
 	switch code {
 	case 200:
-		d.Data["json"] = map[string]interface{}{"error": "200", "err_msg": errMsg, "data": result}
+		d.Data["json"] = map[string]interface{}{"code": 200, "err_msg": errMsg, "data": result}
 	default:
-		d.Data["json"] = map[string]interface{}{"error": "500", "err_msg": errMsg, "data": result}
+		d.Data["json"] = map[string]interface{}{"code": 500, "err_msg": errMsg}
 	}
 	d.ServeJSON()
 }
